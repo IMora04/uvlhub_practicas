c49f298c079022bb079c2f9fb22e412a9d06dae6
Ignacio Mora <ignaciomorap04@gmail.com>
2025-10-28 10:50:11 +0100

    Añadidos tests de la practica 4

--- app/modules/notepad/tests/test_unit.py
+++ app/modules/notepad/tests/test_unit.py
@@ -1,5 +1,12 @@
 import pytest
+from app.modules.notepad.services import NotepadService
+from app.modules.notepad.models import Notepad
+from app import db
+from app.modules.auth.models import User
+from app.modules.conftest import login, logout
+from app.modules.profile.models import UserProfile
 
+notepad_service = NotepadService()
 
 @pytest.fixture(scope='module')
 def test_client(test_client):
@@ -7,18 +14,27 @@ def test_client(test_client):
     Extends the test_client fixture to add additional specific data for module testing.
     """
     with test_client.application.app_context():
-        # Add HERE new elements to the database that you want to exist in the test context.
-        # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
-        pass
+        user_test = User(email="user@example.com", password="test1234")
+        db.session.add(user_test)
+        db.session.commit()
+
+        profile = UserProfile(user_id=user_test.id, name="Name", surname="Surname")
+        db.session.add(profile)
+        db.session.commit()
 
     yield test_client
 
+def test_get_all_notepads_returns_list(test_client):
+    userId = 1
+    result = notepad_service.get_all_by_user(userId)
+    assert isinstance(result, list)
+    assert all(isinstance(t, Notepad) for t in result)
 
-def test_sample_assertion(test_client):
-    """
-    Sample test to verify that the test framework and environment are working correctly.
-    It does not communicate with the Flask application; it only performs a simple assertion to
-    confirm that the tests in this module can be executed.
-    """
-    greeting = "Hello, World!"
-    assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
+def test_create_notepad(test_client):
+    userId = 1
+    initialLen = len(notepad_service.get_all_by_user(userId))
+    createdNotepad = notepad_service.create(title="test", body="test", user_id=userId)
+    newNotepads = notepad_service.get_all_by_user(userId)
+    assert len(newNotepads) == initialLen + 1
+    assert createdNotepad in newNotepads
+    assert createdNotepad.title == "test"
