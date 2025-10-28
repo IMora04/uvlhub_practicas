2d76ed54b2eb11e517c39d964506dda7eae168e7
Ignacio Mora <ignaciomorap04@gmail.com>
2025-10-28 10:51:04 +0100

    Añadido path local de geckodriver para ARM

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,9 +1,8 @@
 import os
-
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
-
+import platform
 
 def initialize_driver():
     options = webdriver.FirefoxOptions()
@@ -12,10 +11,18 @@ def initialize_driver():
     os.makedirs(snap_tmp, exist_ok=True)
     os.environ["TMPDIR"] = snap_tmp
 
-    service = Service(GeckoDriverManager().install())
+    # Detect system architecture
+    arch = platform.machine()
+    if arch in ("aarch64", "arm64"):
+        # Use system ARM64 geckodriver
+        gecko_path = "/usr/local/bin/geckodriver"
+    else:
+        # Use webdriver-manager on x86/x86_64
+        gecko_path = GeckoDriverManager().install()
+
+    service = Service(gecko_path)
     driver = webdriver.Firefox(service=service, options=options)
     return driver
-
 
 def close_driver(driver):
     driver.quit()
